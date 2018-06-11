2a08970d5159b073ee322aed77aa828d1493ba57
Radosław Kowalski <radek.kwlsk@gmail.com>
2018-06-11 18:53:35 +0200

    add page title; update ranges

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 import dash
 
-app = dash.Dash('Sensor Network')
+app = dash.Dash(name='sensornetworks-dash')
+app.title = 'Wireless Sensor Networks'
 
 NODES_RANGE = (10, 150, 100)
 NODES_RANGE2 = (2, 10, 7)
-POOL_RANGE = (3, 12, 10)
-KEYS_RANGE = (1, 10, 5)
+POOL_RANGE = (10, 20, 10)
+KEYS_RANGE = (1, 15, 5)
 DIMENSION_RANGE = (10, 100, 50)
 RANGE_RANGE = (0, 100, 10)
